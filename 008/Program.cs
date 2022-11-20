31d333d13e2a28da2562ab06125780bb2642c413
Ivan_M <molivan@yandex.ru>
2022-11-20 15:43:24 +0400

    треугольник

--- 008/Program.cs
+++ 008/Program.cs
@@ -4,7 +4,7 @@ int xa = 40;
 int ya = 2;
 int xb = 2;
 int yb = 20;
-int xc = 40;
+int xc = 80;
 int yc = 20;
 
 Console.SetCursorPosition(xa, ya);
@@ -16,3 +16,31 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
+int x = xa;
+int y = xb;
+
+int count = 0;
+
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
