cc5189ef7494ff07345453defa3bc9bb9e175fb8
Ivan_M <molivan@yandex.ru>
2022-11-20 13:51:47 +0400

    Treugolnik

---
+++ 008/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int xa = 40;
+int ya = 2;
+int xb = 2;
+int yb = 20;
+int xc = 40;
+int yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
